e038264eca5a3129aca5399b2fd7e8998d0df3ec
tsabunkar <tsabunkar@gmail.com>
2018-06-03 18:19:14 +0530

    configuring mongoose_config.js

--- server/db/mongoose_config.js
+++ server/db/mongoose_config.js
@@ -2,6 +2,9 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/mymongodb'
+//  const url = 'mongodb://tsabunkar:icui4cuNOW@ds245240.mlab.com:45240/mongodbhost' ;
+
+
 mongoose.connect(url);
 //for production env, we are using the mongodb uri as -> process.env.MONGODB_URI
 
